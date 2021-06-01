c2e68c4513e82aed1744146a4a2283cbc593a326
Marika <marikashanahan@gmail.com>
2021-06-01 13:45:39 -0600

    next_palindrom working solution

--- next_palindrome/index.js
+++ next_palindrome/index.js
@@ -2,6 +2,28 @@ export const findNextPalindrome = baseInt => {
   if (typeof(baseInt) !== 'number') return 'Not a valid input, please use an integer';
 
   let original = baseInt.toString().split('');
+  let reverse = [...original].reverse();
+
+  if (original.toString() === reverse.toString()) {
+    original[Math.floor(original.length / 2)] = (parseInt(++original[Math.floor(original.length / 2)])).toString();
+    reverse = [...original].reverse();
+  };
   
-  if (original === baseInt.toString().split('').reverse()) return baseInt;
+  for (let i = 0; i < Math.floor(original.length / 2); i++) {
+    if (original[i] !== reverse[i]) {
+      reverse[i] = original[i]
+    }
+  }
+
+  if (parseInt(reverse.join('')) < baseInt) {
+    original[Math.floor(original.length / 2)] = (parseInt(++original[Math.floor(original.length / 2)])).toString();
+    reverse = [...original].reverse();
+    for (let i = 0; i < Math.floor(original.length / 2); i++) {
+      if (original[i] !== reverse[i]) {
+        reverse[i] = original[i]
+      }
+    }
+  }  
+
+  return parseInt(reverse.join(''));
 }
--- next_palindrome/test.js
+++ next_palindrome/test.js
@@ -1,17 +1,19 @@
-import expect from 'chai';
+import { expect } from 'chai';
 import { findNextPalindrome } from './index.js';
 
-describe('Find Next Palindrom algorithm', () => {
+describe('Find Next Palindrome algorithm', () => {
   it('Should only accept an integer', () => {
-    expect(findNextPalindrome('345')).to.return('Not a valid input, please use an integer');
-    
+    expect(findNextPalindrome('345')).to.deep.equal('Not a valid input, please use an integer');
   })
 
   it('Should return an integer', () => {
     expect(findNextPalindrome(101)).to.be.a('number');
   })
 
-  // it('Should return an integer', () => {
-
-  // })
+  it('If not currently a palindrome, return the NEXT numeric order valid palindrome', () => {
+    expect(findNextPalindrome(100)).to.deep.equal(101);
+    expect(findNextPalindrome(101)).to.deep.equal(111);
+    expect(findNextPalindrome(312)).to.deep.equal(313);
+    expect(findNextPalindrome(316)).to.deep.equal(323);
+  })
 });
